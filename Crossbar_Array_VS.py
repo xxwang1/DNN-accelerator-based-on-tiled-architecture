260a9f9dc8500ba6bb50b912064dac72c03ad9fd
Qiwen Wang <wqw8000@hotmail.com>
2018-12-31 15:26:09 -0500

    User name update.

--- Crossbar_Array_VS.py
+++ Crossbar_Array_VS.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Parameter: Lu Group Differential model
 n1 = 9e-8
 n2 = 15.5
 gamma = 1e-5
@@ -15,15 +16,14 @@ B_p = n1*np.sinh(n2*Vw)
 B_n = n1*np.sinh(n2*(-Vw))
 
 w_avg = 0.03
+###########################################
 
-def device_update(w, num_pulse):
-    for i in range(abs(num_pulse)):
-        if num_pulse > 0:
-            w = w + B_p*(1-w)*pulse_wdith
-        else:
-            w = w + B_n*(1-w)*pulse_wdith
+#Parameter: Algebra model
 
-class Crossbar():
+###########################################
+
+
+class Crossbar_diff():
     @staticmethod
     def device_update(w_in, num_pulse):
         w = w_in
@@ -50,13 +50,35 @@ class Crossbar():
     def get_array(self):
         return self.I_array
 
+
+#class Crossbar_alg():
+#    @staticmethod
+#    def device_update(w_in, num_pulse):
+        
+#        return w
+
+#    def __init__(self, cb_size, new_array):
+#        self.cb_size = cb_size # CB size
+#        self.w_array = np.full((cb_size, cb_size), w_avg)
+#        for ix, iy in np.ndindex(self.w_array.shape):
+#            self.w_array[ix,iy] = self.device_update(self.w_array[ix,iy], new_array[ix,iy])
+        
+
+#    def get_array(self):
+#        return self.I_array
+
+
     #def matmul(self, input_vector):
     #    return np.matmul(input_vector, self.array)
-    
-ini_array = np.array(range(9)).reshape(3,3)
+
+
+size = 5
+
+ini_array = np.array(range(size*size)).reshape(size, size)
 test_array = np.array(ini_array)
-cb_test = Crossbar(3, test_array)
+cb_test = Crossbar_diff(size, test_array)
 output = cb_test.get_array().flatten()
+
 print(output)
 plt.plot(output)
 plt.show()
