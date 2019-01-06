661e7d792b352b1f72db0ed5f66a2898c493e1cf
Qiwen Wang <wangqiwe@umich.edu>
2019-01-06 17:20:53 -0500

    Added algebraic Crossbar_alg class

--- Crossbar_Array_VS.py
+++ Crossbar_Array_VS.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from math import log, exp
 
+###########################################
 #Parameter: Lu Group Differential model
 n1 = 9e-8
 n2 = 15.5
@@ -17,11 +19,6 @@ B_n = n1*np.sinh(n2*(-Vw))
 
 w_avg = 0.03
 ###########################################
-
-#Parameter: Algebra model
-
-###########################################
-
 
 class Crossbar_diff():
     #array[row, col]
@@ -58,34 +55,84 @@ class Crossbar_diff():
         return output
 
 
-#class Crossbar_alg():
-#    @staticmethod
-#    def device_update(w_in, num_pulse):
-        
-#        return w
+###########################################
+#Parameter: Algebra model
+G_min_avg = 2e-6  #average minimal conductance
+G_max_avg = 4e-6  #average maximum conductance
+P_max = 20
+A_avg = P_max/2
+B_avg = (G_max_avg - G_min_avg)/(1-exp(-P_max/A))
+V_read = 1
 
-#    def __init__(self, cb_size, new_array):
-#        self.cb_size = cb_size # CB size
-#        self.w_array = np.full((cb_size, cb_size), w_avg)
-#        for ix, iy in np.ndindex(self.w_array.shape):
-#            self.w_array[ix,iy] = self.device_update(self.w_array[ix,iy], new_array[ix,iy])
-        
+###########################################
 
-#    def get_array(self):
-#        return self.I_array
+class Crossbar_alg():
+    @staticmethod
+    def device_update(G_in, G_min, G_max, A, num_pulse): 
+        #num_pulse > 0 LTP, num_pulse < 0 LTD
+        #Return updated G value
+        G = 0
+        P = 0
+        B = (G_max - G_min)/(1-exp(-P_max/A))
+        if num_pulse > 0: #LTP
+            B = (G_max - G_min)/(1-exp(-P_max/A))
+            P = -A*log(1-(G_in-G_min)/B)
+            if P + num_pulse >= P_max:
+                P = P_max
+            else:
+                P = P + num_pulse
+            G = B*(1-exp(-P/A)) + G_min
+        elif num_pulse == 0:
+            return G_in
+        else: #LTD
+            P = -A*log(1-(G_max-G_in)/B)
+            if P + abs(num_pulse) >= P_max:
+                P = P_max
+            else:
+                P = P + abs(num_pulse)
+            G = -B*(1-exp(-P/A)) + G_max
 
+        return G
 
-    #def matmul(self, input_vector):
-    #    return np.matmul(input_vector, self.array)
+    def __init__(self, cb_size, new_array):
+        self.cb_size = cb_size # CB size
+        self.G_min_array = np.full((cb_size, cb_size), G_min_avg)
+        self.G_max_array = np.full((cb_size, cb_size), G_max_avg)
+        self.A_array = np.full((cb_size, cb_size), A_avg)
+        self.G_array = np.full((cb_size, cb_size), G_min_avg)
+        for ix, iy in np.ndindex(self.G_array.shape):
+            self.G_array[ix,iy] = self.device_update(self.G_array[ix,iy], self.G_min_array[ix,iy], self.G_max_array[ix,iy], self.A_array[ix,iy], new_array[ix,iy])
+
+    def update(self, new_array):
+        for ix, iy in np.ndindex(self.G_array.shape):
+            self.G_array[ix,iy] = self.device_update(self.G_array[ix,iy], self.G_min_array[ix,iy], self.G_max_array[ix,iy], self.A_array[ix,iy], new_array[ix,iy])
+
+    def get_array(self):
+        return self.G_array
+
+    def cal_dot(self, input): 
+        output = np.matmul(input*V_read, self.G_array)
+        return output
 
 
 size = 5
 
-ini_array = np.array(range(size*size)).reshape(size, size)
-test_array = np.array(ini_array)
-cb_test = Crossbar_diff(size, test_array)
-output = cb_test.get_array().flatten()
+#ini_array = np.array(range(size*size)).reshape(size, size)
+#test_array = np.array(ini_array)
+#cb_test = Crossbar_diff(size, test_array)
+#output = cb_test.get_array().flatten()
 
+ini_array = np.array(range(size*size)).reshape(size, size)
+cb_test = Crossbar_alg(size, ini_array)
+
+output = cb_test.get_array().flatten()
+print(output)
+plt.plot(output)
+plt.show()
+
+cb_test2 = Crossbar_alg(size, np.full((size, size),P_max))
+cb_test2.update(-1*ini_array)
+output = cb_test2.get_array().flatten()
 print(output)
 plt.plot(output)
 plt.show()
